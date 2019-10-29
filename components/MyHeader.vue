bef943493fef44c499ced5ea6be49df9b39007e9
Administrator <daoke_li@qq.com>
2019-10-29 21:57:13 +0800

    添加footer

--- components/MyHeader.vue
+++ components/MyHeader.vue
@@ -1,20 +1,23 @@
 <template>
-    <el-row>
-        <el-col :span="4">
-          <a href="http://e-art.top">
-            <img src="../static/img/logo_v0.1_w.png" alt="logo">
-          </a>
-        </el-col>
-        <el-col :span="16">
-            <h1 class="title">
-                阿洛识字
-            </h1>
-        </el-col>
-        <el-col :span="4">
-          <div><output id="answer" >{{result}}</output></div>
+  <div class="fixed-top">
+    <el-row class="w-100">
+      <el-col :span="4">
+        <a href="http://e-art.top">
+          <img src="../static/img/logo_v0.1_w.png" alt="logo">
+        </a>
+      </el-col>
+      <el-col :span="16">
+        <h1 class="title">
+          阿洛识字
+        </h1>
+      </el-col>
+      <el-col :span="4">
+        <div><output id="answer" >{{result}}</output></div>
 
-        </el-col>
+      </el-col>
     </el-row>
+  </div>
+
 </template>
 <script>
   import Bus from '../middleware/BusEvent'
@@ -37,6 +40,7 @@
 </script>
 <style lang="scss">
   .el-row{
+    top: 0;
     padding: .5rem 0;
     background: #409EFF;
     h1 {
